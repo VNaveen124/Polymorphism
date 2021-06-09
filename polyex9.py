e20bf29f37d79e6591f31358b8b8ed8155e4dc4d
NAVEEN KUMAR <75079832+VNaveen124@users.noreply.github.com>
2021-06-09 13:33:16 +0530

    polyex

---
+++ polyex9.py
@@ -0,0 +1,30 @@
+#polyex9.py
+class India:
+	def capital(self):   # original method
+		print("Delhi is the capital of India")
+	def lang(self):    # original method
+		print("Hindi / Mixed Lang Indian can speack")
+	def  type(self):   # original method
+		print("India is a developping.... Country")
+class USA:
+	def capital(self):  
+		print("WashingTon is the capital of USA")
+	def lang(self):   
+		print("English Lang, Americans can speack")
+	def  type(self):  
+		print("USA is a developped Country")
+
+class Country:
+	@staticmethod
+	def dispCountryInfo(obj):
+		print("----------------------------------------------------")
+		obj.capital()
+		obj.lang()
+		obj.type()
+		print("----------------------------------------------------")
+
+#main program
+uo=USA()
+ind=India()
+Country.dispCountryInfo(uo)
+Country.dispCountryInfo(ind)
